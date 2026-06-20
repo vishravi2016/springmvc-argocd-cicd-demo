c688d8306a5cd1192b6444d4486b804c83be3100
vishravi2016 <ravikumar.k1@tataelxsi.co.in>
2026-06-20 20:31:45 +0530

    created simple spring mvc and dockerfile

---
+++ src/test/java/in/elxsi/ravi/SpringbootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package in.elxsi.ravi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
